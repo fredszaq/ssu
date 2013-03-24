1f0b2171cf4ef823fb90a053a845ed0bc98b84a8
Thibaut Lorrain <fredszaq@gmail.com>
2013-03-24 16:33:16 +0100

    completed the toXml method of RssItem and added the missing building methods

--- src/main/scala/RssTest.scala
+++ src/main/scala/RssTest.scala
@@ -3,20 +3,28 @@ import java.util.Locale
 import com.tlorrain.ssu.rss.RssItem
 import scala.xml.PrettyPrinter
 import scala.xml.XML
+import scala.language.postfixOps
 
 object RssTest extends App {
 
-  override def main(args: Array[String]) { 
-    val myItems= ("hello","world") :: ("You are", "a bitch") :: Nil
-    
-    val rss= RssFeed("some title<hello>world</hello>", "http://someli.nk", "some description<heloo>world</heloo>")
-    
-    val rss2=rss.copy(copyright=Some("toto"))
-    val extractedLocalValue = rss2 withCopyright "Fredszaq" withItems (myItems map ( item => RssItem(item._1, item._2))) toXml
-    
-   extractedLocalValue.toString
-    XML.save("/tmp/test", extractedLocalValue/*, xmlDecl=true*/)
-    println(new PrettyPrinter(100,4).format(XML.loadString(extractedLocalValue.toString)))
+  override def main(args: Array[String]) {
+    val myItems = ("hello", "world") :: ("You are", "a bitch") :: Nil
+
+    val rss = RssFeed("some title<hello>world</hello>", "http://someli.nk", "some description<heloo>world</heloo>")
+
+    val rss2 = rss.copy(copyright = Some("toto"))
+    val feed = (rss2
+      withCopyright "Fredszaq"
+      withItems (myItems map (item => RssItem(item._1, item._2)
+        withLink "http://some.other.li.nk"
+        withEnclosure ("http://u.rl", 666, "application/doom")
+        withAuthor ("Fredszaq")
+        withComments ("http://yetanother.url")
+        withCategory ("super category"))) toXml)
+
+    feed.toString
+    XML.save("/tmp/test", feed , xmlDecl=true )
+    println(new PrettyPrinter(100, 4).format(XML.loadString(feed.toString)))
   }
 
 }
--- src/main/scala/com/tlorrain/ssu/rss/RssItem.scala
+++ src/main/scala/com/tlorrain/ssu/rss/RssItem.scala
@@ -6,15 +6,47 @@ case class RssItem(title: String,
   author: Option[String] = None, // TODO email
   category: Option[String] = None, // TODO optional attribute "domain"
   comments: Option[String] = None, // TODO url
-  enclosure: Option[Tuple3[String, Int, String]] = None, // TODO proper type ?
+  enclosure: Option[Tuple3[String, Long, String]] = None, // TODO proper type ?
   guid: Option[String] = None,
   pubDate: Option[Long] = None, // TODO date
   source: Option[Tuple2[String, String]] = None // TODO proper type
   ) {
 
+  def withLink(link: Option[String]) = copy(link = link)
+  def withLink(link: String) = copy(link = Some(link))
+
+  def withAuthor(author: Option[String]) = copy(author = author)
+  def withAuthor(author: String) = copy(author = Some(author))
+
+  def withCategory(category: Option[String]) = copy(category = category)
+  def withCategory(category: String) = copy(category = Some(category))
+
+  def withComments(comments: Option[String]) = copy(comments = comments)
+  def withComments(comments: String) = copy(comments = Some(comments))
+
+  def withEnclosure(enclosure: Option[Tuple3[String, Long, String]]) = copy(enclosure = enclosure)
+  def withEnclosure(enclosure: Tuple3[String, Long, String]) = copy(enclosure = Some(enclosure))
+
+  def withGuid(guid: Option[String]) = copy(guid = guid)
+  def withGuid(guid: String) = copy(guid = Some(guid))
+
+  def withPubDate(pubDate: Option[Long]) = copy(pubDate = pubDate)
+  def withPubDate(pubDate: Long) = copy(pubDate = Some(pubDate))
+
+  def withSource(source: Option[Tuple2[String, String]]) = copy(source = source)
+  def withSource(source: Tuple2[String, String]) = copy(source = Some(source))
+
   def toXml = <item>
                 <title>{ title }</title>
                 <description>{ description }</description>
+                { (for (link <- link) yield <link>{ link }</link>).getOrElse("") }
+                { (for (author <- author) yield <author>{ author }</author>).getOrElse("") }
+                { (for (category <- category) yield <category>{ category }</category>).getOrElse("") }
+                { (for (comments <- comments) yield <comments>{ comments }</comments>).getOrElse("") }
+                { (for ((url, length, typ) <- enclosure) yield <enclosure url={ url } length={ length.toString } type={ typ }/>).getOrElse("") }
+                { (for (guid <- guid) yield <guid>{ guid }</guid>).getOrElse("") }
+                { (for (pubDate <- pubDate) yield <pubDate>{ pubDate }</pubDate>).getOrElse("") }
+                { (for ((url, title) <- source) yield <source url={ url }>{ title }</source>).getOrElse("") }
               </item>
 
 }

f3c6c2de7d2bd6dca1a57b1892f7d21ccdfdc0a1
Thibaut Lorrain <fredszaq@gmail.com>
2013-06-27 23:36:28 +0200

    fix timezone problems in test

--- src/test/scala/com/tlorrain/ssu/rss/RssFeedToXmlTest.scala
+++ src/test/scala/com/tlorrain/ssu/rss/RssFeedToXmlTest.scala
@@ -4,11 +4,14 @@ import org.scalatest.FunSuite
 import java.util.Locale
 import org.scalatest.matchers.ShouldMatchers
 import java.net.URL
+import java.util.TimeZone
 
 class RssFeedToXmlTest extends FunSuite with ShouldMatchers {
 
   val items = ("title1", "content1", new URL("http://link1.com"), "author1@site1.com") :: ("title2", "content2", new URL("http://link2.com"), "author2@site2.com") :: Nil
 
+  TimeZone.setDefault(TimeZone.getTimeZone("GMT")); // just to be sure date format in test always use this one
+  
   test("generate correct XML (simple version)") {
 
     val rss = (
@@ -59,7 +62,7 @@ class RssFeedToXmlTest extends FunSuite with ShouldMatchers {
           withPubDate 1357924680
           withSource RssSource(new URL("http://www.sourceurl.eu"), "Very cool site")))).toXml
 
-    val expected = <rss version="2.0"><channel><title>some title</title><link>http://someli.nk</link><description>some description</description><language>en</language><copyright>SSU</copyright><managingEditor>somebody@somewhere.com</managingEditor><webMaster>somebodyElse@somewhere.com</webMaster><pubDate>Thu, 1 Jan 1970 01:00:00 +0100</pubDate><lastBuildDate>Thu, 15 Jan 1970 07:56:07 +0100</lastBuildDate><category domain="http://www.yetanotherlink.org">IT</category><generator>Scala Syndication Utils</generator><docs>http://www.rssboard.org/rss-specification</docs><cloud>TODO</cloud><ttl>60</ttl><image><title>Some image</title><url>http://imageurl.info/img.png</url><link>http://imageurl.info/img.png</link><width>144</width><height>400</height><description>Some description</description></image><rating>TODO</rating><textInput>TODO</textInput><skipHours><hour>0</hour><hour>1</hour><hour>12</hour><hour>15</hour></skipHours><skipDays><day>Saturday</day><day>Sunday</day></skipDays><item><title>title1</title><description>content1</description><link>http://link1.com</link><author>author1@site1.com</author><category domain="http://www.yetanotherlink.org">IT</category><comments>http://www.comments-url.net</comments><enclosure url="http://www.localtion.biz" length="7839" type="image/png"/><guid>http://link1.com</guid><pubDate>Fri, 16 Jan 1970 18:12:04 +0100</pubDate><source url="http://www.sourceurl.eu">Very cool site</source></item><item><title>title2</title><description>content2</description><link>http://link2.com</link><author>author2@site2.com</author><category domain="http://www.yetanotherlink.org">IT</category><comments>http://www.comments-url.net</comments><enclosure url="http://www.localtion.biz" length="7839" type="image/png"/><guid>http://link2.com</guid><pubDate>Fri, 16 Jan 1970 18:12:04 +0100</pubDate><source url="http://www.sourceurl.eu">Very cool site</source></item></channel></rss>
+    val expected = <rss version="2.0"><channel><title>some title</title><link>http://someli.nk</link><description>some description</description><language>en</language><copyright>SSU</copyright><managingEditor>somebody@somewhere.com</managingEditor><webMaster>somebodyElse@somewhere.com</webMaster><pubDate>Thu, 1 Jan 1970 00:00:00 +0000</pubDate><lastBuildDate>Thu, 15 Jan 1970 06:56:07 +0000</lastBuildDate><category domain="http://www.yetanotherlink.org">IT</category><generator>Scala Syndication Utils</generator><docs>http://www.rssboard.org/rss-specification</docs><cloud>TODO</cloud><ttl>60</ttl><image><title>Some image</title><url>http://imageurl.info/img.png</url><link>http://imageurl.info/img.png</link><width>144</width><height>400</height><description>Some description</description></image><rating>TODO</rating><textInput>TODO</textInput><skipHours><hour>0</hour><hour>1</hour><hour>12</hour><hour>15</hour></skipHours><skipDays><day>Saturday</day><day>Sunday</day></skipDays><item><title>title1</title><description>content1</description><link>http://link1.com</link><author>author1@site1.com</author><category domain="http://www.yetanotherlink.org">IT</category><comments>http://www.comments-url.net</comments><enclosure url="http://www.localtion.biz" length="7839" type="image/png"/><guid>http://link1.com</guid><pubDate>Fri, 16 Jan 1970 17:12:04 +0000</pubDate><source url="http://www.sourceurl.eu">Very cool site</source></item><item><title>title2</title><description>content2</description><link>http://link2.com</link><author>author2@site2.com</author><category domain="http://www.yetanotherlink.org">IT</category><comments>http://www.comments-url.net</comments><enclosure url="http://www.localtion.biz" length="7839" type="image/png"/><guid>http://link2.com</guid><pubDate>Fri, 16 Jan 1970 17:12:04 +0000</pubDate><source url="http://www.sourceurl.eu">Very cool site</source></item></channel></rss>
 
     rss toString () should equal(expected toString ())
 
